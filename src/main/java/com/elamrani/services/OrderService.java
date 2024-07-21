d28f816e23526c63377174ca2203157a4b0211ee
TomElAmrani <mohammed.elamrani8@usmba.ac.ma>
2024-07-21 18:12:01 +0100

    Add the service layer of the Order

---
+++ src/main/java/com/elamrani/services/OrderService.java
@@ -0,0 +1,19 @@
+package com.elamrani.services;
+
+import java.util.List;
+
+import com.elamrani.dtos.OrderDTO;
+import com.elamrani.dtos.OrderResponse;
+
+public interface OrderService {
+	
+	OrderDTO placeOrder(String emailId, Long cartId, String paymentMethod);
+	
+	OrderDTO getOrder(String emailId, Long orderId);
+	
+	List<OrderDTO> getOrdersByUser(String emailId);
+	
+	OrderResponse getAllOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+	
+	OrderDTO updateOrder(String emailId, Long orderId, String orderStatus);
+}
